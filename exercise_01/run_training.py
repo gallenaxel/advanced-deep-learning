0a79d0621ddbd598c8eb9c4f6dac9b02ee57142d
Philipp Rincke <philipp.rincke@cern.ch>
2025-03-25 14:20:06 +0100

    Save best model

--- exercise_01/run_training.py
+++ exercise_01/run_training.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 
 
 
-num_epochs = 3
+num_epochs = 10
 
 def main():
     spectra, labels = get_data() # spectra shape (8914, 16384)
@@ -29,8 +29,10 @@ def main():
     model.to(device)
     
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    criterion = nn.functional.mse_loss
     
-    losses = []
+    train_losses = []
+    val_losses = []
     
     train_batch_loader, val_batch_loader, test_batch_loader = get_batch_loaders(spectra, labels)
     
@@ -42,16 +44,31 @@ def main():
             batch_x = batch_x.to(device)
             batch_y = batch_y.to(device)
             forward_pass_outputs = model(batch_x)
-            loss = nn.functional.mse_loss(forward_pass_outputs.ravel(), batch_y.ravel())
+            loss = criterion(forward_pass_outputs.ravel(), batch_y.ravel())
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             
             train_loss += loss.item()
             
-        losses.append(train_loss/(batch_idx+1))
+        train_losses.append(train_loss/len(train_batch_loader))
+        
+        model.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for batch_x, batch_y in val_batch_loader:
+                predictions = model(batch_x)
+                loss = criterion(predictions, batch_y)
+                val_loss += loss.item()
+        val_loss /= len(val_batch_loader)
+        
+        if epoch > 0:
+            if val_loss < val_losses[-1]:
+                torch.save(model.state_dict(), "saved_models/best_model.pth")
+
+        val_losses.append(val_loss)
             
-    print(losses)
+    print(train_losses)
 
 if __name__ == "__main__":
     main()
