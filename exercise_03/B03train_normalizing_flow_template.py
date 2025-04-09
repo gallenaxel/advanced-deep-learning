eee742af3d91cff724f0e649e2b68c732c1ba989
Philipp Rincke <philipp.rincke@cern.ch>
2025-04-09 19:21:20 +0200

    Apply ruff formating

--- exercise_03/B03train_normalizing_flow_template.py
+++ exercise_03/B03train_normalizing_flow_template.py
@@ -1,5 +1,5 @@
-
 import sys
+
 sys.path.append("../")
 
 import time
@@ -30,8 +30,6 @@ learning_rate = 0.8e-5
 batch_size = 32
 
 
-
-
 # Call the function to get normalized data
 spectra, labels = get_data("train")
 
@@ -43,7 +41,6 @@ class TinyCNNEncoder(nn.Module):
         super(TinyCNNEncoder, self).__init__()
 
         self.model = nn.Sequential(
-            
             nn.Linear(spectra.shape[1], latent_dimension),
         )
 
@@ -69,8 +66,10 @@ def nf_loss(inputs, batch_labels, model):
     torch.Tensor
         The computed loss value.
     """
-    log_pdfs = model.log_pdf_evaluation(batch_labels, inputs) # get the probability of the labels given the input data
-    loss = -log_pdfs.mean() # take the negative mean of the log probabilities
+    log_pdfs = model.log_pdf_evaluation(
+        batch_labels, inputs
+    )  # get the probability of the labels given the input data
+    loss = -log_pdfs.mean()  # take the negative mean of the log probabilities
     return loss
 
 
@@ -107,17 +106,17 @@ class CombinedModel(nn.Module):
 
         super().__init__()
 
-        # we define a 3-d PDF over Euclidean spae (e3)
+        # we define a 3-d PDF over Euclidean space (e3)
         # using recommended settings (https://github.com/thoglu/jammy_flows/issues/5 scroll down)
         opt_dict = {}
         opt_dict["t"] = {}
-        if (nf_type == "diagonal_gaussian"):
+        if nf_type == "diagonal_gaussian":
             opt_dict["t"]["cov_type"] = "diagonal"
             flow_defs = "t"
-        elif (nf_type == "full_gaussian"):
+        elif nf_type == "full_gaussian":
             opt_dict["t"]["cov_type"] = "full"
             flow_defs = "t"
-        elif (nf_type == "full_flow"):
+        elif nf_type == "full_flow":
             opt_dict["t"]["cov_type"] = "full"
             flow_defs = "gggt"
         else:
@@ -131,8 +130,13 @@ class CombinedModel(nn.Module):
         self.nf_type = nf_type
 
         # 3d PDF (e3) with ggggt flow structure. Four Gaussianation-flow (https://arxiv.org/abs/2003.01941) layers ("g") and an affine flow ("t")
-        self.pdf = jammy_flows.pdf("e3", flow_defs, options_overwrite=opt_dict,
-                                   amortize_everything=True, amortization_mlp_use_custom_mode=True)
+        self.pdf = jammy_flows.pdf(
+            "e3",
+            flow_defs,
+            options_overwrite=opt_dict,
+            amortize_everything=True,
+            amortization_mlp_use_custom_mode=True,
+        )
 
         # get the number of flow parameters
         num_flow_parameters = self.pdf.total_number_amortizable_params
@@ -160,9 +164,11 @@ class CombinedModel(nn.Module):
         log_pdf : torch.Tensor
             The evaluated log PDF for the given target labels and input data.
         """
-        latent_intermediate = self.encoder(input_data)  # get the flow parameters from the CNN encoder
+        latent_intermediate = self.encoder(
+            input_data
+        )  # get the flow parameters from the CNN encoder
 
-        if (self.nf_type == "full_flow"):
+        if self.nf_type == "full_flow":
             # convert to double. Double precision is needed for the Gaussianization flow. This is for numerical stability.
             if fp64_on_cpu:  # MPS does not support double precision, therefore we need to run the flow on the CPU
                 latent_intermediate = latent_intermediate.cpu().to(torch.float64)
@@ -172,7 +178,9 @@ class CombinedModel(nn.Module):
                 target_labels = target_labels.to(torch.float64)
 
         # evaluate the log PDF at the target labels
-        log_pdf, _, _ = self.pdf(target_labels, amortization_parameters=latent_intermediate)
+        log_pdf, _, _ = self.pdf(
+            target_labels, amortization_parameters=latent_intermediate
+        )
         return log_pdf
 
     def sample(self, flow_params, samplesize_per_batchitem=1000):
@@ -189,26 +197,31 @@ class CombinedModel(nn.Module):
         Returns
         -------
         tensor
-            A tensor of shape (B, S, D) where B is the batch dimension, S is the number of samples, 
+            A tensor of shape (B, S, D) where B is the batch dimension, S is the number of samples,
             and D is the dimension of the target space for the samples.
         """
         # for full flow we need to convert to double precision for the normalizing flow
         # for numerical stability
-        if (self.nf_type == "full_flow"):
+        if self.nf_type == "full_flow":
             # convert to double
-            if fp64_on_cpu: # MPS does not support double precision, therefore we need to run the flow on the CPU
+            if fp64_on_cpu:  # MPS does not support double precision, therefore we need to run the flow on the CPU
                 flow_params = flow_params.cpu().to(torch.float64)
             else:
                 flow_params = flow_params.to(torch.float64)
 
-        batch_size = flow_params.shape[0] # get the batch size
+        batch_size = flow_params.shape[0]  # get the batch size
         # sample from the normalizing flow
-        repeated_samples, _, _, _ = self.pdf.sample(amortization_parameters=flow_params.repeat_interleave(
-            samplesize_per_batchitem, dim=0), allow_gradients=False)
+        repeated_samples, _, _, _ = self.pdf.sample(
+            amortization_parameters=flow_params.repeat_interleave(
+                samplesize_per_batchitem, dim=0
+            ),
+            allow_gradients=False,
+        )
 
         # reshape the samples to be grouped by batch item
         reshaped_samples = repeated_samples[:, None, :].view(
-            batch_size, samplesize_per_batchitem, -1)
+            batch_size, samplesize_per_batchitem, -1
+        )
 
         return reshaped_samples
 
@@ -228,25 +241,30 @@ class CombinedModel(nn.Module):
         Returns
         -------
         torch.Tensor
-            A tensor of size (B, D*2) where the first half (size D) are the means, 
+            A tensor of size (B, D*2) where the first half (size D) are the means,
             the second half (another D) are the standard deviations.
         """
-        flow_params=self.encoder(input_data)
-        samples=self.sample(flow_params, samplesize_per_batchitem=samplesize_per_batchitem)
+        flow_params = self.encoder(input_data)
+        samples = self.sample(
+            flow_params,
+            samplesize_per_batchitem=samplesize_per_batchitem,
+        )
 
         # form mean along dim 1 (samples)
-        means=samples.mean(dim=1)
+        means = samples.mean(dim=1)
         # form std along dim 1 (samples)
-        std_deviations=samples.std(dim=1)
+        std_deviations = samples.std(dim=1)
 
         # return means and std deviations as a concatenated tensor along dim 1
         return torch.cat([means, std_deviations], dim=1)
 
-    def visualize_pdf(self, input_data, filename, samplesize=1000, batch_index=0, truth=None):
+    def visualize_pdf(
+        self, input_data, filename, samplesize=1000, batch_index=0, truth=None
+    ):
         """
         Visualizes the probability density function (PDF) of the given input data using a normalizing flow model.
 
-        The function generates samples from the normalizing flow (using the sample() function) 
+        The function generates samples from the normalizing flow (using the sample() function)
         and plots the histogram of the samples together with a Gaussian approximation.
 
         Parameters
@@ -267,7 +285,7 @@ class CombinedModel(nn.Module):
         None
         """
         # pick out one input from batch
-        input_bitem = input_data[batch_index:batch_index+1]
+        input_bitem = input_data[batch_index : batch_index + 1]
 
         # get the flow parameters (by passing the input data through the CNN encoder network)
         flow_params = self.encoder(input_bitem)
@@ -289,25 +307,31 @@ class CombinedModel(nn.Module):
         fig, axdict = plt.subplots(3, 1)
         for dim_ind in range(3):
             # plot the histogram of the samples
-            axdict[dim_ind].hist(samples[:, dim_ind], color="k", density=True,
-                                 bins=50, alpha=0.5, label="density based on samples")
+            axdict[dim_ind].hist(
+                samples[:, dim_ind],
+                color="k",
+                density=True,
+                bins=50,
+                alpha=0.5,
+                label="density based on samples",
+            )
 
             # plot the Gaussian approximation
             min_sample = samples[:, dim_ind].min()
             max_sample = samples[:, dim_ind].max()
             xvals = np.linspace(min_sample, max_sample, 1000)
             yvals = norm.pdf(xvals, loc=mean[dim_ind], scale=std[dim_ind])
-            axdict[dim_ind].plot(xvals, yvals, color="green",
-                                 label="Gaussian approximation")
+            axdict[dim_ind].plot(
+                xvals, yvals, color="green", label="Gaussian approximation"
+            )
 
             # plot the true value if it is given
-            if (truth is not None):
+            if truth is not None:
                 true_value = truth[dim_ind].cpu().item()
-                axdict[dim_ind].axvline(
-                    true_value, color="red", label="true value")
+                axdict[dim_ind].axvline(true_value, color="red", label="true value")
 
             # plot the legend only for the first panel
-            if (dim_ind == 0):
+            if dim_ind == 0:
                 axdict[dim_ind].legend()
 
         plt.savefig(filename)
@@ -316,8 +340,11 @@ class CombinedModel(nn.Module):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-normalizing_flow_type", default="diagonal_gaussian",
-                        choices=["diagonal_gaussian", "full_gaussian", "full_flow"])
+    parser.add_argument(
+        "-normalizing_flow_type",
+        default="diagonal_gaussian",
+        choices=["diagonal_gaussian", "full_gaussian", "full_flow"],
+    )
     args = parser.parse_args()
     print("Using normalizing flow type ", args.normalizing_flow_type)
 
